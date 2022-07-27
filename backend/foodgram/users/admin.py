072504146138950cd97d2519fc7bfcc4663aae72
Galina Volkova <earlinndrow@gmail.com>
2022-07-28 00:19:19 +0300

    Add user and subscription models to admin panel

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Subscription, User
+
+
+class UserAdmin(admin.ModelAdmin):
+    """Class to customize Users display in admin panel."""
+    list_display = ['pk', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'date_joined']
+    search_fields = ['username', 'first_name', 'last_name', 'email']
+    list_filter = ['username', 'email', 'is_staff', 'date_joined']
+    empty_value_display = '-empty-'
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    """Class to customize Subscriptions display in admin panel."""
+    list_display = ['user', 'author']
+    search_fields = ['user', 'author']
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Subscription, SubscriptionAdmin)
