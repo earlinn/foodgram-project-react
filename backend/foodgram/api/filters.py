9922d3b99124738ad58b194dc7ac02349b7a6ad6
Galina Volkova <earlinndrow@gmail.com>
2022-08-06 17:29:40 +0300

    Make double filtering by ingredient name

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,15 +1,30 @@
+from django.db.models import BooleanField, ExpressionWrapper, Q
 from django_filters import rest_framework as rf_filters
 from recipes.models import Ingredient, Recipe
 
 
 class IngredientFilter(rf_filters.FilterSet):
-    """"""
+    """Class for filtering ingredients by their names."""
 
-    name = rf_filters.CharFilter(field_name='name', lookup_expr='icontains')
+    name = rf_filters.CharFilter(method='startswith_contains_union_method')
 
     class Meta:
         model = Ingredient
         fields = ['name']
+
+    def startswith_contains_union_method(self, queryset, name, value):
+        if not bool(value):
+            return queryset
+        startswith_lookup = '__'.join([name, 'istartswith'])
+        contains_lookup = '__'.join([name, 'icontains'])
+        return queryset.filter(
+            Q(**{startswith_lookup: value}) | Q(**{contains_lookup: value})
+        ).annotate(
+            is_start=ExpressionWrapper(
+                Q(**{startswith_lookup: value}),
+                output_field=BooleanField()
+            )
+        ).order_by('-is_start')
 
 
 class RecipeFilter(rf_filters.FilterSet):
@@ -25,11 +40,13 @@ class RecipeFilter(rf_filters.FilterSet):
         fields = ['tags', 'author', 'is_favorited', 'is_in_shopping_cart']
 
     def recipe_boolean_methods(self, queryset, name, value):
-        if bool(value) and not self.request.user.is_anonymous:
-            user = self.request.user
-            recipe_ids = [
-                r.pk for r in queryset if getattr(r, name)(user) == value]
-            if recipe_ids:
-                return queryset.filter(pk__in=recipe_ids)
-            return queryset.none()
-        return queryset
+        if not bool(value):
+            return queryset
+        user = self.request.user
+        if user.is_anonymous:
+            return queryset
+        recipe_ids = [
+            r.pk for r in queryset if getattr(r, name)(user) == value]
+        if recipe_ids:
+            return queryset.filter(pk__in=recipe_ids)
+        return queryset.none()
