3439ccc49368c2eb01fa725cc2f097d62a7ed6fc
Galina Volkova <earlinndrow@gmail.com>
2022-08-10 00:47:04 +0300

    Add deploy step to workflow

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -25,13 +25,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', default='key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ["http://localhost/*"]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost/*",
+    "62.84.120.133"
+]
 
 # Application definition
 
